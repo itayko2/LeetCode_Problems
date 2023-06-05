cc2d73e3d9073252a066c3018977238d94852151
itayko2 <59698200+itayko2@users.noreply.github.com>
2023-06-05 18:01:45 +0300

    Update merge_intervals.py

--- merge_intervals.py
+++ merge_intervals.py
@@ -2,18 +2,13 @@
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
         sorted_intervals = sorted(intervals, key=lambda x: x[0])
-        def helper(vals):
-            if (len(vals) <= 1):
-                return vals
+        merged_intervals = []
+        
+        for interval in sorted_intervals:
+            if not merged_intervals or interval[0] > merged_intervals[-1][1]:
+                merged_intervals.append(interval)
             else:
-                if (vals[0][0] <= vals[1][0] <= vals[0][1]):
-                    merged = [vals[0][0], max(vals[1][1],vals[0][1])]
-                    return helper([merged] + vals[2:])
-                else:
-                    return [vals[0]] + helper(vals[1:])
-
-        return helper(sorted_intervals)
-
-            
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])
+        
+        return merged_intervals
